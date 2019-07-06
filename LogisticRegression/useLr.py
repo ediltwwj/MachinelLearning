21528aca9b7a91db81bf7df4d27e65f109d50c77
13967 <1396748139@qq.com>
2019-07-06 11:25:04 +0800

    绘制鸢尾花逻辑回归模型的不规则决策边界

--- LogisticRegression/useLr.py
+++ LogisticRegression/useLr.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from KNN.modelSplit import train_test_split
 from LogisticRegression.logisticRegression import LogisticRegression
+from LogisticRegression.drawDecisionBoundary import plot_decision_boundary
+
 
 def x2(x1):
     """以p=0.5为边界推导决策边界函数"""
@@ -34,6 +36,13 @@ if __name__ == "__main__":
     plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red')
     plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue')
     plt.plot(x1_plot, x2_plot)
+
+    plt.show()
+
+    # 绘制不规则决策边界
+    plot_decision_boundary(log_reg, axis=[4, 7.5, 1.5, 4.5])
+    plt.scatter(X[y == 0, 0], X[y == 0, 1])
+    plt.scatter(X[y == 1, 0], X[y == 1, 1])
 
     plt.show()
 
