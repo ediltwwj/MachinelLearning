7646f23087796cbfe8ebe4672859999c62a305b1
13967 <1396748139@qq.com>
2019-07-30 15:55:44 +0800

    绘制决策边界

--- LogisticRegression/drawDecisionBoundary.py
+++ LogisticRegression/drawDecisionBoundary.py
@@ -12,18 +12,18 @@ def plot_decision_boundary(model, axis):
         np.linspace(axis[2], axis[3], int((axis[3] - axis[2]) * 100)).reshape(-1, 1),
     )
     # ravel()方法将高维数组降为一维数组，c_[]将两个数组以列的形式拼接起来，形成矩阵
-    X_grid_matrix = np.c_[X0.ravel(), X1.ravel()]
+    X_new = np.c_[X0.ravel(), X1.ravel()]
 
     # 通过训练好的逻辑回归模型，预测平面上这些点的分类
-    y_predict = model.predict(X_grid_matrix)
-    y_predict_matrix = y_predict.reshape(X0.shape)
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(X0.shape)
 
     # 设置色彩表
     from matplotlib.colors import ListedColormap
-    my_colormap = ListedColormap(['#0000CD', '#40E0D0', '#FFFF00'])
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
 
     # 绘制等高线，并且填充等高区域的颜色
-    plt.contourf(X0, X1, y_predict_matrix, linewidth=5, cmap=my_colormap)
+    plt.contourf(X0, X1, zz, linewidth=5, cmap=custom_cmap)
 
 
 
